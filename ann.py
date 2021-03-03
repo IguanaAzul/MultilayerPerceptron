95a684e5fb3dc38e15f0941abd48163b5e625c2a
IguanaAzul <ramon.griffo98@gmail.com>
2021-03-02 21:01:19 -0300

    activation functions

--- ann.py
+++ ann.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def step(x):
     x = np.array(x)
-    x[x < 0] = 0
-    x[x >= 0] = 1
+    x[x <= 0] = 0
+    x[x > 0] = 1
     return x
 
 
@@ -19,18 +19,26 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 
-function = {"step": step, "sigmoid": sigmoid}
+def not_implemented(name):
+    def ni(x):
+        print("Activation function ", name, " not implemented")
+        print("Using relu instead")
+        return relu(x)
+    return ni
+
+
+function = {"step": step, "sigmoid": sigmoid, "relu": relu}
 
 
 class Layer:
     def __init__(self, n_inputs, n_neurons, activation="step"):
         self.n_inputs = n_inputs
-        self.activation = function[activation]
+        self.activation = function[activation] if activation in function.keys() else not_implemented(activation)
         self.input_weights = np.random.rand(n_inputs, n_neurons) * 2 - 1
         self.biases = np.random.rand(n_neurons) * 2 - 1
 
     def forward(self, inputs):
-        return np.dot(inputs, self.input_weights) + self.biases
+        return self.activation(np.dot(inputs, self.input_weights) + self.biases)
 
 
 class Network:
